4cd187e326a5d5d5a5fbb1cbb303f5d86ccb7075
Rjw <jia1235590@gmail.com>
2026-04-28 10:54:38 +0900

    업로드

--- django_docker/settings.py
+++ django_docker/settings.py
@@ -75,9 +75,13 @@ WSGI_APPLICATION = 'django_docker.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME' : 'rdsdb',
+        'USER' : 'django',
+        'PASSWORD' : os.getenv('LOCAL_RDSDB_PASSWORD'),
+        'HOST' : 'localhost',
+        'PORT' : '3306',
+    },
 }
 
 
